34e13f814339b1c0f3ef2451215152a45c7c048f
vysakhav <vysakh_av@outlook.com>
2020-09-24 20:28:36 +0100

    ChatBot - Intial Commit

---
+++ src/graphnode.cpp
@@ -0,0 +1,61 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <iostream>
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.push_back(std::make_unique<GraphEdge>(*edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    std::cout << "Move the ownership" << std::endl;
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
